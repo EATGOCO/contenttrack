4a0e53a91d79d117c8b9c364732629f8b0f31029
Ronny Wang <srwang@eatgo.com>
2014-10-01 12:09:17 +0800

    給內部使用，因此不過 google openid ，直接相信使用者輸入

--- webdata/controllers/LoginController.php
+++ webdata/controllers/LoginController.php
@@ -17,23 +17,14 @@ class LoginController extends Pix_Controller
 
     public function googledoneAction()
     {
-        $consumer = $this->getGoogleConsumer();
-        $return_to = 'http://' . $_SERVER['HTTP_HOST'] . '/login/googledone';
-        $response = $consumer->complete($return_to);
-
-        if ($response->status == Auth_OpenID_CANCEL) {
-            return $this->alert('取消登入', '/');
-        }
-        if ($response->status == Auth_OpenID_FAILURE) {
-            return $this->alert('登入失敗: ' . $response->message, '/');
-        }
-
-        $ax = new Auth_OpenID_AX_FetchResponse();
-        $obj = $ax->fromSuccessResponse($response);
-        $email = $obj->data['http://axschema.org/contact/email'][0];
+      $email = $_POST['email'] . '@eatgo.com';
 
         if (!$user = User::search(array('user_name' => 'google://' . $email))->first()) {
+          list($name, $domain) = explode('@', $email);
+          if ('eatgo.com' != $domain) {
             return $this->alert('您不在管理名單中', '/');
+          }
+          $user = User::insert(array('user_name' => 'google://' . $email));
         }
         Pix_Session::set('user_id', $user->user_id);
         return $this->redirect('/');
@@ -41,33 +32,6 @@ class LoginController extends Pix_Controller
 
     public function googleAction()
     {
-        $consumer = $this->getGoogleConsumer();
-        $url = 'https://www.google.com/accounts/o8/id';
-        $auth_request = $consumer->begin($url);
-
-        if (!$auth_request) {
-            return $this->alert('Authentication error, not a valid OpenID', '/');
-        }
-
-        $ax = new Auth_OpenID_AX_FetchRequest;
-        $ax->add(Auth_OpenID_AX_AttrInfo::make('http://axschema.org/contact/email',2,1, 'email'));
-        $auth_request->addExtension($ax);
-
-        $pape_request = new Auth_OpenID_PAPE_Request(null);
-        $auth_request->addExtension($pape_request);
-
-        $form_id = 'openid_message';
-        $form_html = $auth_request->htmlMarkup('http://' . $_SERVER['HTTP_HOST'], 'http://' . $_SERVER['HTTP_HOST'] . '/login/googledone',
-                                               false, array('id' => $form_id));
-
-        // Display an error if the form markup couldn't be generated;
-        // otherwise, render the HTML.
-        if (Auth_OpenID::isFailure($form_html)) {
-            $this->alert("Could not redirect to server: " . $form_html->message, '/');
-        } else {
-            print $form_html;
-            return $this->noview();
-        }
     }
 
     public function logoutAction()
